876c45112a4a07ddea3e4e4098dc475c83d93d0f
xiaofengjohn <xiaofenglijohn@gmail.com>
2016-04-05 15:47:34 -0700

    Item-Based and User-Based demo

--- src/main/java/com/projecta/ItemBasedRecommendation.java
+++ src/main/java/com/projecta/ItemBasedRecommendation.java
@@ -1,43 +1,62 @@
 package com.projecta;
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
-import org.apache.mahout.cf.taste.common.TasteException;
-import org.apache.mahout.cf.taste.impl.model.file.*;
-import org.apache.mahout.cf.taste.impl.neighborhood.*;
-import org.apache.mahout.cf.taste.impl.recommender.*;
-import org.apache.mahout.cf.taste.impl.similarity.*;
-import org.apache.mahout.cf.taste.model.*;
-import org.apache.mahout.cf.taste.neighborhood.*;
-import org.apache.mahout.cf.taste.recommender.*;
-import org.apache.mahout.cf.taste.similarity.*;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.CachingRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.GenericItemSimilarity;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 public class ItemBasedRecommendation {
-	public static void main(String[] args) {		
-		try{	
-		
-			
-			//Step 1:- Input CSV file (CSV file should be in userID, itemID, preference) format
 
-			//DataModel dm = new FileDataModel(new File("filePath"));
-			
-			//Step 2:- Create UserSimilarity or ItemSimilarity Matrix
-			
-			//Step 3:- Create UserNeighbourHood object. (No Need to create ItemNeighbourHood object while creating 
-			//Item based Recommendation)
-			
-			//Step 4:- Create object of UserBasedRecommender or ItemBasedRecommender
-			
-			//Step 5:- Call the Generated Recommender in previous step to getting 
-			//recommendation for particular user or Item	
-			
-			
-		}
-		catch (Exception e) {
+	public static void main(String[] args) {
+		try {
+
+			// Step 1:- Input CSV file (CSV file should be in userID, itemID,
+			// preference) format
+
+			// DataModel dm = new FileDataModel(new File("filePath"));
+
+			// Step 2:- Create UserSimilarity or ItemSimilarity Matrix
+
+			// Step 3:- Create UserNeighbourHood object. (No Need to create
+			// ItemNeighbourHood object while creating
+			// Item based Recommendation)
+
+			// Step 4:- Create object of UserBasedRecommender or
+			// ItemBasedRecommender
+
+			// Step 5:- Call the Generated Recommender in previous step to
+			// getting
+			// recommendation for particular user or Item
+
+			DataModel model = new FileDataModel(new File("/Users/xiaofengli/documents/cmpe239/lab1/lab1.csv"));
+			Collection<GenericItemSimilarity.ItemItemSimilarity> correlations = new ArrayList<GenericItemSimilarity.ItemItemSimilarity>();
+			ItemSimilarity itemSimilarity = new GenericItemSimilarity(
+					correlations);
+			Recommender recommender = new GenericItemBasedRecommender(model, itemSimilarity);
+			Recommender cachingRecommender = new CachingRecommender(recommender);
+			List<RecommendedItem> recommendations = cachingRecommender.recommend(2, 1);
+			for (RecommendedItem item : recommendations) {
+				System.out.println(item);
+			}
+		} catch (Exception e) {
 			System.out.println("There was an error.");
 			e.printStackTrace();
-		}		
-		
 		}
+	}
+
 }
--- src/main/java/com/projecta/UserBasedRecommendation.java
+++ src/main/java/com/projecta/UserBasedRecommendation.java
@@ -1,43 +1,56 @@
 package com.projecta;
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.util.List;
 
-import org.apache.mahout.cf.taste.common.TasteException;
-import org.apache.mahout.cf.taste.impl.model.file.*;
-import org.apache.mahout.cf.taste.impl.neighborhood.*;
-import org.apache.mahout.cf.taste.impl.recommender.*;
-import org.apache.mahout.cf.taste.impl.similarity.*;
-import org.apache.mahout.cf.taste.model.*;
-import org.apache.mahout.cf.taste.neighborhood.*;
-import org.apache.mahout.cf.taste.recommender.*;
-import org.apache.mahout.cf.taste.similarity.*;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 public class UserBasedRecommendation {
-	public static void main(String[] args) {		
-		try{	
-		
-			
-			//Step 1:- Input CSV file (CSV file should be in userID, itemID, preference) format
 
-			//DataModel dm = new FileDataModel(new File("filePath"));
+	public static void main(String[] args) {
+		try {
+
+			// Step 1:- Input CSV file (CSV file should be in userID, itemID,
+			// preference) format
+
+			// DataModel dm = new FileDataModel(new File("filePath"));
+
+			// Step 2:- Create UserSimilarity or ItemSimilarity Matrix
+
+			// Step 3:- Create UserNeighbourHood object. (No Need to create
+			// ItemNeighbourHood object while creating
+			// Item based Recommendation)
+
+			// Step 4:- Create object of UserBasedRecommender or
+			// ItemBasedRecommender
+
+			// Step 5:- Call the Generated Recommender in previous step to
+			// getting
+			// recommendation for particular user or Item
 			
-			//Step 2:- Create UserSimilarity or ItemSimilarity Matrix
-			
-			//Step 3:- Create UserNeighbourHood object. (No Need to create ItemNeighbourHood object while creating 
-			//Item based Recommendation)
-			
-			//Step 4:- Create object of UserBasedRecommender or ItemBasedRecommender
-			
-			//Step 5:- Call the Generated Recommender in previous step to getting 
-			//recommendation for particular user or Item	
 			
 			
-		}
-		catch (Exception e) {
+			DataModel model = new FileDataModel(new File("/Users/xiaofengli/documents/cmpe239/lab1/lab1.csv"));
+			UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+			UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
+			UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+			List<RecommendedItem> recommendations = recommender.recommend(2, 3);
+			for (RecommendedItem item : recommendations) {
+				System.out.println(item);
+			}
+			
+		} catch (Exception e) {
 			System.out.println("There was an error.");
 			e.printStackTrace();
-		}		
-		
 		}
+	}
+
 }
