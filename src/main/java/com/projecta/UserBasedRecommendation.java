5d3c349d10d13ff3ff23b434991d3f77eda718ec
xiaofengjohn <xiaofenglijohn@gmail.com>
2016-04-13 15:10:58 -0700

    Selected a appropriate user with user id equals 6.

--- src/main/java/com/projecta/UserBasedRecommendation.java
+++ src/main/java/com/projecta/UserBasedRecommendation.java
@@ -14,6 +14,10 @@ import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 public class UserBasedRecommendation {
+	
+	private static final long USER_ID = 6L;
+	
+	private static final int RECOMMENDED_ITEM_NUM = 3;
 
 	public static void main(String[] args) {
 		try {
@@ -21,7 +25,7 @@ public class UserBasedRecommendation {
 			UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
 			UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
 			UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
-			List<RecommendedItem> recommendations = recommender.recommend(2, 4);
+			List<RecommendedItem> recommendations = recommender.recommend(USER_ID, RECOMMENDED_ITEM_NUM);
 			for (RecommendedItem item : recommendations) {
 				System.out.println(item);
 			}
