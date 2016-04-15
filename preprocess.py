ffef5c7d9eed7243936825e1e6c3367c80ec9016
Yutao Hou <chrishou118@gmail.com>
2016-04-15 14:49:50 -0700

    add some comment

--- preprocess.py
+++ preprocess.py
@@ -1,11 +1,15 @@
 import csv
 
+# open original file for read
 with open('movienight.csv', 'rb') as f:
 	reader = csv.reader(f)
+
+	# create output file
 	with open('movienight_2.csv', mode='wb') as outfile:
 		writer = csv.writer(outfile)
 		title = reader.next()
 		count = 1;
+        # iterater row by row, column by column, and write to output file
 		for row in reader:
 			for i in range(5, 32):
 				# print count, title[i], row[i]
